5d4949f9d241bc848a75a05271663a4d5c3a3af4
sasidhar <bodasingisasidhar9999@gmail.com>
2023-07-05 22:18:16 +0530

    s1

---
+++ java03_literals/src/com/dl/one/Eg1.java
@@ -0,0 +1,42 @@
+package com.dl.one;
+
+public class Eg1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		System.out.println(a);
+		
+		float f1 =10.0f;
+		System.out.println(f1);
+		double d1 = 10.0d;
+		System.out.println(d1);
+		
+		boolean b1 = true;
+		System.out.println(b1);
+		
+		char ch1 = 'A';
+		System.out.println(ch1);
+		char ch2 = 98;
+		System.out.println(ch2);
+		char ch3 = '\u0000';
+		System.out.println(ch3);
+		
+		String username = "sasi";
+		System.out.println(username);
+		
+		//binary
+		int i = 0b0110;
+		System.out.println(i);
+		//octal
+		int k = 07676;
+		System.out.println(k);
+		//hexa
+		int j = 0x0000;
+		System.out.println(j);
+		
+		
+		
+	}
+
+}
